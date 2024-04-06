6046b1d1f76ef45811608b380afdcdf5b462f800
so1eeee <soljeong821@gmail.com>
2024-04-06 17:45:26 +0900

    solve 2018

---
+++ src/doit/datastructure/Main2018.java
@@ -0,0 +1,23 @@
+package doit.datastructure;
+
+import java.util.Scanner;
+
+public class Main2018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int j, sum = 0, cnt = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            j = i;
+            while(sum < n){
+                sum += j;
+                if (sum == n) {
+                    cnt++;
+                }
+                j++;
+            }
+            sum = 0;
+        }
+        System.out.println(cnt + 1);
+    }
+}
